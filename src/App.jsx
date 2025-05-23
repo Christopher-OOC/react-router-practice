bff12a77827e5d021d43d3f59bdbee72400b9a33
Christopher-OOC <olojedechristopher24@gmail.com>
2025-05-23 11:30:57 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
 import DashboardMain from "./components/main/DashboardMain";
 
+const URL = "http://localhost:8000/user";
+
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(function () {
+    async function fetchUser() {
+      setIsLoading(true);
+      setError("");
+      try {
+        const res = await fetch(URL);
+        const data = await res.json();
+        console.log(data);
+        setUser(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchUser();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/app" element={<Dashboard />}>
           <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<DashboardMain />} />
+          <Route
+            path="dashboard"
+            element={
+              <DashboardMain user={user} isLoading={isLoading} error={error} />
+            }
+          />
           <Route path="products" element={<p>Products</p>} />
           <Route path="cart" element={<p>Cart</p>} />
           <Route path="orders" element={<p>Orders</p>} />
---
+++ src/components/main/DashboardMain.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
+import styles from "./DashboardMain.module.css";
+
+const URL = "http://localhost:8000/user";
+
+function DashboardMain({ user, isLoading, error }) {
+  // const [user, setUser] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState("");
+
+  // useEffect(function () {
+  //   async function fetchUser() {
+  //     setIsLoading(true);
+  //     setError("");
+  //     try {
+  //       const res = await fetch(URL);
+  //       const data = await res.json();
+  //       console.log(data);
+  //       setUser(data);
+  //     } catch (error) {
+  //       setError(error.message);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+
+  //   fetchUser();
+  // }, []);
+
+  return (
+    <div className={styles.content}>
+      {isLoading && <Spinner />}
+      {!isLoading && !error && <p>Welcome {user.name}</p>}
+    </div>
+  );
+}
+
+export default DashboardMain;
